5fef6823d2a70476610df26605aa9ca4f8a50c3c
Alexander <142725378+gingerbreadman11@users.noreply.github.com>
2024-12-09 14:40:22 +0100

    Create run.py

---
+++ run.py
@@ -0,0 +1,9 @@
+import os
+import sys
+from subprocess import call
+
+# Change directory to the app folder
+os.chdir('Version_3')
+
+# Run the Streamlit app
+call(['streamlit', 'run', 'app.py'] + sys.argv[1:])
